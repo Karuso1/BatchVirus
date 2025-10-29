fae150391e5167c48dcdfb5b9d5e8c1e41a770c1
Kartik Pawar <karuman2013@gmail.com>
2025-10-29 16:29:28 +0530

    update: preferring a powershell command rather than creating a temp file

--- src/popupcheck.bat
+++ src/popupcheck.bat
@@ -1,18 +1,5 @@
 :: Quick batch file to check if the popup works
 
 @echo off
-:: Create the popup window
-set "VBS_FILE=%temp%\warning_temp_%random%.vbs"
-
-echo x=MsgBox("This script contains malicious content, run at your own risk. The author is not responsible for any damage.", 48 + 4, "Warning") > "%VBS_FILE%"
-echo If x=7 Then WScript.Quit >> "%VBS_FILE%"
-
-wscript //nologo "%VBS_FILE%"
-if errorlevel 1 goto :Abort
-
-:: Clean up the temporary VBS file
-del "%VBS_FILE%" >nul 2>&1
-
-:Abort
-echo Operation cancelled by user. Exiting.
+powershell -NoProfile -Command "[void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); $r = [System.Windows.Forms.MessageBox]::Show('This script contains malicious content. Run at your own risk. The author is not responsible for any damage.','Warning',[System.Windows.Forms.MessageBoxButtons]::YesNo,[System.Windows.Forms.MessageBoxIcon]::Exclamation); if($r -eq 'No'){ exit 1 }"
 pause
